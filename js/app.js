5135d9b62c8aa7910f9b18fae80daa3097edf88f
noahsb96 <noahsb96@yahoo.com>
2022-07-09 00:52:24 -0700

    added javascript functions

--- js/app.js
+++ js/app.js
@@ -31,42 +31,99 @@
             // tomagotchi dies from old age after 400 seconds
                 // alert says 'tomagotchi name' died from old age
                 // death ends program and gives option to restart game
-
-const name = prompt('Name your tomagotchi!', 'Name')
+let hunger = document.querySelector('#hunger')
+let sleepiness = document.querySelector('#sleep')
+let boredom = document.querySelector('#bored')
+let age = document.querySelector('#age')
+let name = prompt('Name your tomagotchi!', 'Name')
+let buttons = document.querySelector('.buttons')
+let backgroundImage = document.body.style.backgroundImage
 
 class Pet {
-    constructor(name) {
-        name = this.name
-        hunger = 0
-        sleepiness = 0
-        boredom = 0
-        age = 0
+    constructor(name, hunger, sleepiness, boredom, age) {
+        this.name = name
+        this.hunger = hunger
+        this.sleepiness = sleepiness
+        this.boredom = boredom
+        this.age = age
     }
 
-    hungry() {
-        this.hunger += 1
-    }
-
-    sleepy() {
-        this.sleepiness += 1
-    }
+    begin() {
+        let startTimer = setInterval(() => {
+            if(this.hunger >= 1 && this.sleepiness >= 1 && this.boredom >= 1) {
+                this.hunger--
+                hunger.innerHTML = this.hunger
+                this.sleepiness--
+                sleepiness.innerHTML = this.sleepiness
+                this.boredom--
+                boredom.innerHTML = this.boredom
+            } 
     
-    bored() {
-        this.boredom += 1
+            else if(this.hunger <= 0 || this.sleepiness <= 0 || this.boredom <= 0) {
+                alert(`${name} has gone to Tomagotchi heaven`)
+                clearInterval(startTimer)
+                document.querySelector('#pet').src='../ghost-clipart-transparent-background-808673.png'
+            } else {
+
+            }
+        
+        }, 3000)
+    }
+
+    feed() {
+        this.hunger++
+        hunger.innerHTML = this.hunger
+    }
+
+    sleep() {
+        this.sleepiness++
+        sleepiness.innerHTML = this.sleepiness
+    }
+
+    play() {
+        this.boredom++
+        boredom.innerHTML = this.boredom
+    }
+
+    lightsOff() {
+        document.body.style.backgroundImage="url('../lightsoff.png')"
+        function lightsOn() {
+            document.body.style.backgroundImage="url('../background.jpg')"
+        }
+        setTimeout(lightsOn, 1000)
     }
 
     older() {
-        this.age += 1
+        let ageClock = setInterval(() => {
+            this.age++
+
+            if (this.age === 10) {
+                document.querySelector('#pet').src='../NicePng_tamagotchi-png_3497120.png'
+            }
+            if (this.age === 20) {
+                document.querySelector('#pet').src='../Tomagotchi-Mini-Project-/pngwing.com.png'
+            }
+            if (this.age === 30) {
+                document.querySelector('#pet').src='../SeekPng.com_tamagotchi-png_3497548.png'
+            }
+            if (this.age === 40) {
+                document.querySelector('#pet').src='../pngegg.png'
+            }
+            if (this.age === 50) {
+                document.querySelector('#pet').src='../kisspng-lovelin-portable-network-graphics-image-character-tamagotchi-mxcharacter-list-tamagotchi-wiki-fand-5d1337e1cc34f4.6405991715615405778364.png'
+                clearInterval(ageClock)
+            }
+            if(this.hunger >= 10 || this.sleepiness >= 10 || this.boredom >= 10) {
+                clearInterval(ageClock)
+            }
+        }, 3000)
     }
 }
 
-function begin() {
-    let name = prompt('Name your Tomagotchi!', 'Name')
-    const tomagotchi = new Pet(name)
-    setInterval(tomagotchi.hungry, 5000)
-    setInterval(tomagotchi.sleepy, 10000)
-    setInterval(tomagotchi.bored, 7000)
-    setInterval(tomagotchi.older, 10000)
-}
+let tomagotchi = new Pet(`${name}`, 5, 7, 9 0)
 
-document.getElementById('begin').addEventListener('click', begin)
+document.getElementById('begin').addEventListener('click',(e)=>{tomagotchi.older(), tomagotchi.begin()})
+document.getElementById('give food').addEventListener('click', (e)=>{tomagotchi.feed()})
+document.getElementById('go to sleep').addEventListener('click',(e)=>{tomagotchi.sleep()})
+document.getElementById('play').addEventListener('click', (e)=>{tomagotchi.play()})
+document.getElementById('lights').addEventListener('click',(e)=>{tomagotchi.lightsOff()})
