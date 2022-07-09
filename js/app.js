61e202cc390ceae66df20b7e5f8d6129f6156df0
noahsb96 <noahsb96@yahoo.com>
2022-07-09 10:21:24 -0700

    Game works!

--- js/app.js
+++ js/app.js
@@ -35,13 +35,12 @@ let hunger = document.querySelector('#hunger')
 let sleepiness = document.querySelector('#sleep')
 let boredom = document.querySelector('#bored')
 let age = document.querySelector('#age')
-let name = prompt('Name your tomagotchi!', 'Name')
 let buttons = document.querySelector('.buttons')
 let backgroundImage = document.body.style.backgroundImage
 
 class Pet {
     constructor(name, hunger, sleepiness, boredom, age) {
-        this.name = name
+        this.name = prompt('Name your tomagotchi!', 'Name')
         this.hunger = hunger
         this.sleepiness = sleepiness
         this.boredom = boredom
@@ -52,17 +51,18 @@ class Pet {
         let startTimer = setInterval(() => {
             if(this.hunger >= 1 && this.sleepiness >= 1 && this.boredom >= 1) {
                 this.hunger--
-                hunger.innerHTML = this.hunger
+                hunger.innerHTML = (`Hunger: ${this.hunger}`)
                 this.sleepiness--
-                sleepiness.innerHTML = this.sleepiness
+                sleepiness.innerHTML = (`Sleepiness: ${this.sleepiness}`)
                 this.boredom--
-                boredom.innerHTML = this.boredom
+                boredom.innerHTML = (`Boredom: ${this.boredom}`)
+                age.innerHTML = (`age: ${this.age}`)
             } 
     
             else if(this.hunger <= 0 || this.sleepiness <= 0 || this.boredom <= 0) {
-                alert(`${name} has gone to Tomagotchi heaven`)
+                document.getElementById('pet').src='/Tomagotchi-Mini-Project-/ghost-clipart-transparent-background-808673.png'
+                alert(`${this.name} has gone to Tomagotchi heaven`)
                 clearInterval(startTimer)
-                document.querySelector('#pet').src='../ghost-clipart-transparent-background-808673.png'
             } else {
 
             }
@@ -72,58 +72,58 @@ class Pet {
 
     feed() {
         this.hunger++
-        hunger.innerHTML = this.hunger
+        hunger.innerHTML = (`Hunger: ${this.hunger}`)
     }
 
     sleep() {
         this.sleepiness++
-        sleepiness.innerHTML = this.sleepiness
+        sleepiness.innerHTML = (`Sleepiness: ${this.sleepiness}`)
+        document.body.style.backgroundImage="url(/Tomagotchi-Mini-Project-/lightsoff.png)"
+        function lightsOn() {
+            document.body.style.backgroundImage="url(/Tomagotchi-Mini-Project-/Background.jpg)"
+        }
+        setTimeout(lightsOn, 1000)
     }
 
     play() {
         this.boredom++
-        boredom.innerHTML = this.boredom
-    }
-
-    lightsOff() {
-        document.body.style.backgroundImage="url('../lightsoff.png')"
-        function lightsOn() {
-            document.body.style.backgroundImage="url('../background.jpg')"
-        }
-        setTimeout(lightsOn, 1000)
+        boredom.innerHTML = boredom.innerHTML = (`Boredom: ${this.boredom}`)
     }
 
     older() {
         let ageClock = setInterval(() => {
             this.age++
-
             if (this.age === 10) {
-                document.querySelector('#pet').src='../NicePng_tamagotchi-png_3497120.png'
+                document.getElementById('pet').src='/Tomagotchi-Mini-Project-/NicePng_tamagotchi-png_3497120.png'
+                alert(`${this.name} has hatched and is now a baby!`)
             }
             if (this.age === 20) {
-                document.querySelector('#pet').src='../Tomagotchi-Mini-Project-/pngwing.com.png'
+                document.getElementById('pet').src='/Tomagotchi-Mini-Project-/pngwing.com.png'
+                alert(`${this.name} has grown from a baby to a child!`)
             }
             if (this.age === 30) {
-                document.querySelector('#pet').src='../SeekPng.com_tamagotchi-png_3497548.png'
+                document.getElementById('pet').src='/Tomagotchi-Mini-Project-/com_tamagotchi-png_3497548.png'
+                alert(`${this.name} has grown from a child to a teenager!`)
             }
             if (this.age === 40) {
-                document.querySelector('#pet').src='../pngegg.png'
+                document.getElementById('pet').src='/Tomagotchi-Mini-Project-/pngegg.png'
+                alert(`${this.name} has grown from a teenager to an adult!`)
             }
             if (this.age === 50) {
-                document.querySelector('#pet').src='../kisspng-lovelin-portable-network-graphics-image-character-tamagotchi-mxcharacter-list-tamagotchi-wiki-fand-5d1337e1cc34f4.6405991715615405778364.png'
+                document.getElementById('pet').src='/Tomagotchi-Mini-Project-/kisspng-lovelin-portable-network-graphics-image-character-tamagotchi-mxcharacter-list-tamagotchi-wiki-fand-5d1337e1cc34f4.6405991715615405778364.png'
+                alert(`${this.name} has grown from an adult to an senior!`)
                 clearInterval(ageClock)
             }
-            if(this.hunger >= 10 || this.sleepiness >= 10 || this.boredom >= 10) {
+            if(this.hunger <= 0 || this.sleepiness <= 0 || this.boredom <= 0) {
                 clearInterval(ageClock)
             }
         }, 3000)
     }
 }
 
-let tomagotchi = new Pet(`${name}`, 5, 7, 9 0)
+let tomagotchi = new Pet(`${this.name}`, 10, 10, 10, 0)
 
-document.getElementById('begin').addEventListener('click',(e)=>{tomagotchi.older(), tomagotchi.begin()})
+document.getElementById("begin").addEventListener("click",(e)=>{tomagotchi.older(), tomagotchi.begin()})
 document.getElementById('give food').addEventListener('click', (e)=>{tomagotchi.feed()})
 document.getElementById('go to sleep').addEventListener('click',(e)=>{tomagotchi.sleep()})
 document.getElementById('play').addEventListener('click', (e)=>{tomagotchi.play()})
-document.getElementById('lights').addEventListener('click',(e)=>{tomagotchi.lightsOff()})
