9847b748df0067fd657de2923d78f70f9e9fc445
noahsb96 <noahsb96@yahoo.com>
2022-07-09 10:41:06 -0700

    added modal box

--- js/app.js
+++ js/app.js
@@ -127,3 +127,28 @@ document.getElementById("begin").addEventListener("click",(e)=>{tomagotchi.older
 document.getElementById('give food').addEventListener('click', (e)=>{tomagotchi.feed()})
 document.getElementById('go to sleep').addEventListener('click',(e)=>{tomagotchi.sleep()})
 document.getElementById('play').addEventListener('click', (e)=>{tomagotchi.play()})
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("HTP");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
